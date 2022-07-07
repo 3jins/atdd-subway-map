76e2f7adf356e8d6a3f1ebbdb1440c9073f3fec1
Sejin Jeon <jinaidy93@gmail.com>
2022-07-07 19:22:51 +0900

    step1

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,7 +38,7 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
+        final Map<String, String> params = new HashMap<>();
         params.put("name", "강남역");
 
         ExtractableResponse<Response> response =
@@ -52,7 +53,7 @@ public class StationAcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
+        final List<String> stationNames =
                 RestAssured.given().log().all()
                         .when().get("/stations")
                         .then().log().all()
@@ -66,6 +67,33 @@ public class StationAcceptanceTest {
      * Then 2개의 지하철역을 응답 받는다
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void getStations() {
+        // given
+        final List<String> stationNamesToInsert = List.of("선릉역", "역삼역");
+        stationNamesToInsert.forEach((stationName) -> RestAssured.given().log().all()
+                .body(Map.of("name", stationName))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract());
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // then
+        final List<String> respondedNames = response.body().jsonPath().getList("name", String.class);
+        respondedNames.containsAll(stationNamesToInsert);
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -73,5 +101,29 @@ public class StationAcceptanceTest {
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        final String stationNameToBeDeleted = "선릉역";
+        final String stationNameToBeRemained = "역삼역";
+        // given
+        final List<String> stationNamesToInsert = List.of(stationNameToBeDeleted, stationNameToBeRemained);
+        stationNamesToInsert.forEach((stationName) -> RestAssured.given().log().all()
+                .body(Map.of("name", stationName))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract());
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+        final String name = response.body().jsonPath().get("$[0].name");
+        System.out.println("!!!!" + name);
+        // TODO: jsonPath 사용법 익히고 마저 구현...
+    }
 
 }
